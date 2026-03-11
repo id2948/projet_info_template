d1453bd9113e514d859376b7902002660e47775d
id2948n <nathan.tiatia@eleve.ensai.fr>
2026-03-11 11:27:15 +0000

    coach

--- src/coach.py
+++ src/coach.py
@@ -2,5 +2,7 @@ from individu import Individu
 
 
 class Coach(Individu):
-    """Modélise les coachs parmi les individus dans la compétition.
-    "
+    """Modélise les coachs parmi les individus dans la compétition."""
+
+    def __init__(self, id: str, nom: str, prenom: str) -> None:
+        Individu.__init__(self, id, nom, prenom)
